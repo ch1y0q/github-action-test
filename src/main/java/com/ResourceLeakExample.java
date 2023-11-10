0cc3744becd482cd26aa82e12983d4d4ff849f9b
ch1y0q <qiyue2001@gmail.com>
2023-11-10 14:28:28 +0800

    build depends on analyze

--- src/main/java/com/ResourceLeakExample.java
+++ src/main/java/com/ResourceLeakExample.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-// public class ResourceLeakExample {
+ public class ResourceLeakExample {
 //    public static void main(String[] args) {
 //        BufferedReader reader = null;
 //        try {
